b4cf7a739b90a2adbe0cec5e6c30f62fc9ad1996
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-10-07 19:10:07 -0700

    update PhotoListItem.jsx to match predefined styles in PhotoListItem.scss

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,15 +4,17 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = ({ sampleDataForPhotoListItem }) => {
   return (
-    <>
-      <img src={sampleDataForPhotoListItem.imageSource} />
-      <img src={sampleDataForPhotoListItem.profile} />
-      <div>
-        {sampleDataForPhotoListItem.username}
-        <br />
-        {sampleDataForPhotoListItem.location.city}, {sampleDataForPhotoListItem.location.country}
+    <div className="photo-list__item">
+      <img className="photo-list__image" src={sampleDataForPhotoListItem.imageSource} />
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={sampleDataForPhotoListItem.profile} />
+        <div className="photo-list__user-info">
+          {sampleDataForPhotoListItem.username}
+          <br />
+          <span className="photo-list__user-location">{sampleDataForPhotoListItem.location.city}, {sampleDataForPhotoListItem.location.country}</span>
+        </div>
       </div>
-    </>
+    </div>
   )
 };
 

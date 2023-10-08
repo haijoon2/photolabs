52e247ac510e00f1a2fcc8a8825d0c8917a810ce
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-10-07 21:40:54 -0700

    add TopicList.jsx to App.jsx and make it render mock data in TopicList.jsx

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -2,17 +2,11 @@ import React from "react";
 
 import "../styles/TopicListItem.scss";
 
-const sampleDataForTopicListItem = {
-  id: "1",
-  slug: "topic-1",
-  label: "Nature",
-};
-
-const TopicListItem = () => {
+const TopicListItem = ({ title }) => {
   return (
     <div className="topic-list__item">
       <span>
-        {sampleDataForTopicListItem.label}
+        {title}
       </span>
     </div>
   );
